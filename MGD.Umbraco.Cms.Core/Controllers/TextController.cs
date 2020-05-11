a7f73126846080ce27fa089e813f346ee10752c8
Alexander Karlsson <alexander.karlsson@wipcore.se>
2020-05-11 11:26:39 +0200

    sln file

---
+++ MGD.Umbraco.Cms.Core/Controllers/TextController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using MGD.Umbraco.Cms.Core.Helpers;
+using MGD.Umbraco.Cms.Core.Mappers;
+using MGD.Umbraco.Cms.ModelsBuilder.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi;
+using MGD.StructuredDictionary;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Models;
+
+namespace MGD.Umbraco.Cms.Core.Controllers
+{
+    [PluginController("CmsApi")]
+    public class TextController : UmbracoApiController
+    {
+        [System.Web.Http.HttpGet]
+        public JToken GetAllTexts(string culture = "en-us")
+        {
+            CultureHelper.Set(culture);
+
+            var rootDictionaryItems = Current.Services.LocalizationService.GetRootDictionaryItems();
+            var texts = new Dictionary<string,string>();
+            foreach (var rootDictionaryItem in rootDictionaryItems)
+            {
+                var value = GetDictionaryValue(rootDictionaryItem, culture);
+                if (value != null)
+                {
+                    texts.Add(value.Item1, value.Item2);
+                }
+
+                var descendants = Current.Services.LocalizationService.GetDictionaryItemDescendants(rootDictionaryItem.Key);
+                foreach (var descendant in descendants)
+                {
+                    value = GetDictionaryValue(descendant, culture);
+                    if (value != null)
+                    {
+                        texts.Add(value.Item1, value.Item2);
+                    }
+                }
+            }
+
+            // Json settings
+            var jsonSerializerSettings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+
+            var data = Newtonsoft.Json.JsonConvert.SerializeObject(texts, jsonSerializerSettings);
+            return JToken.Parse(data);
+        }
+
+        /// <summary>
+        /// TODO: We must lock this endpoint out
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="fallback"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        [System.Web.Http.HttpGet]
+        public string GetText(string key, string fallback = "", string culture = "en-us")
+        {
+            CultureHelper.Set(culture);
+            var text = TextHelper.Translate(key, fallback);
+            return text;
+        }
+
+        private Tuple<string, string> GetDictionaryValue(IDictionaryItem dictionaryItem, string culture)
+        {
+            var key = dictionaryItem.ItemKey;
+            var value = dictionaryItem.Translations.FirstOrDefault(t => t?.Language.IsoCode.ToLower() == culture.ToLower())?.Value;
+            if (!string.IsNullOrEmpty(value))
+            {
+                return new Tuple<string, string>(key, value);
+            }
+
+            return null;
+        }
+    }
+}
--- MGD.Umbraco.Cms.Core/CultureHelper.cs
+++ MGD.Umbraco.Cms.Core/CultureHelper.cs
@@ -7,7 +7,7 @@ namespace MGD.Umbraco.Cms.Core
     {
         public static void Set(string culture)
         {
-            if (!string.IsNullOrEmpty(culture))
+            if (Validate(culture) && !string.IsNullOrEmpty(culture))
             {
                 Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
                 Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
@@ -17,6 +17,19 @@ namespace MGD.Umbraco.Cms.Core
         public static string Get()
         {
             return Thread.CurrentThread.CurrentCulture.Name;
+        }
+
+        private static bool Validate(string culture)
+        {
+            if (!string.IsNullOrEmpty(culture))
+            {
+                if (culture.Contains("-") && culture.Length == 5) // en-us = 5 characters
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
